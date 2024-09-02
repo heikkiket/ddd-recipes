1477e0b2652549e35ab2d8f15281ad167b86b685
Heikki Ketoharju <heikki.ketoharju@gofore.com>
2024-09-02 13:45:45 +0300

    Add a small demo-program demonstrating the current functionality

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("test")
+import Recipe from './domain/Recipe.js'
+import Ingredient from './domain/Ingredient.js'
+
+const tomatoSoup = new Recipe()
+tomatoSoup.addIngredient(new Ingredient("tomato"))
+tomatoSoup.addIngredient(new Ingredient("salt"))
+
+console.log("This test program now just prints out ingredients of tomato soup:\n")
+console.log(printRecipe(tomatoSoup))
+
+function printRecipe(recipe) {
+  return "Tomato soup\n" +
+    "-------------\n" + 
+  printIngredients(recipe.listIngredients())
+}
+
+function printIngredients(ingredients) {
+  return ingredients.map(ingredient => "   " + ingredient.name + "\n").join("")
+}
